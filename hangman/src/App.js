9ab73c884782999fccf8b3966eff98d6e76bfbb6
Christopher Sharif <christophersharif@gmail.com>
2022-02-24 19:17:39 -0500

    Started Main Screen

--- hangman/src/App.js
+++ hangman/src/App.js
@@ -1,23 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  let alphabet = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="page">
+      <header className='header'>Hangman</header>
+      <h1 className='how-to-play'>How to Play: Guess a letter, if it's
+        in the word, it will appear in the correct spot.
+        If not, the hangman will start to appear. Guess
+        the word before you hang the man!
+      </h1>
+      <div className='letters'>
+        {alphabet.map(letter =>
+          <div className='alphabet'>{letter}</div>
+        )}
+      </div>
     </div>
   );
 }
